466f0af054b4e14cf54c8cf2b0ab5802763b0840
Pavan Ingale <pavaningale7849@gmail.com>
2026-03-22 18:19:59 +0530

    4th commit

--- test/test_example1.py
+++ test/test_example1.py
@@ -1,7 +1,13 @@
-from src.mat_operation import add
+from src.mat_operation import add, subtract
 
 def test_add():
     assert add(1, 2) == 3
     assert add(-1) == 0
     assert add(0, 0) == 0
     assert add(2.5, 3.5) == 6.0 
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+    assert subtract(0, 1) == -1
+    assert subtract(10, 5) == 5
+    assert subtract(3.5, 1.5) == 2.0
